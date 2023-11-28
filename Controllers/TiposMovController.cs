65410d8bd8db39b7b1fe0cf74c40832a3042b679
ReynaldoSG <124290617+ReynaldoSG@users.noreply.github.com>
2023-11-28 09:17:32 -0600

    jhvjh

--- Controllers/TiposMovController.cs
+++ Controllers/TiposMovController.cs
@@ -65,7 +65,7 @@ namespace marcatel_api.Controllers
 
         
         [HttpPut("Update")]
-        public JsonResult UpdateTiposMov([FromQuery] UpdateTiposMovModel tiposmov)
+        public JsonResult UpdateTiposMov([FromBody] UpdateTiposMovModel tiposmov)
         {
             var objectResponse = Helper.GetStructResponse();
             try
@@ -93,7 +93,7 @@ namespace marcatel_api.Controllers
         }
 
         [HttpDelete("Delete")]
-        public JsonResult DeleteTiposMov([FromQuery] DeleteTiposMovModel tiposmov)
+        public JsonResult DeleteTiposMov([FromBody] DeleteTiposMovModel tiposmov)
         {
             var objectResponse = Helper.GetStructResponse();
             try
