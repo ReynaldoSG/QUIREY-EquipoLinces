2b33ff189c5424a0ec0a70e5695f5ca4278a7880
ReynaldoSG <124290617+ReynaldoSG@users.noreply.github.com>
2024-02-12 08:47:50 -0600

    Login User

--- Models/UsuariosModel.cs
+++ Models/UsuariosModel.cs
@@ -3,31 +3,37 @@ namespace marcatel_api.Models
 {
     public class GetUsuariosModel
     {
-        public int Id {get; set;}
-        public string Nombre {get; set;}
-        public string Contrasena {get; set;}
-        public string Rol {get; set;}
-        public string Usuario {get; set;}
-        public DateTime FechaAct {get; set;}
-        public DateTime FechaReg {get; set;}
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Contrasena { get; set; }
+        public string Rol { get; set; }
+        public string Usuario { get; set; }
+        public DateTime FechaAct { get; set; }
+        public DateTime FechaReg { get; set; }
+    }
+    public class GetLoginUserModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Contrasena { get; set; }
     }
     public class InsertUsuariosModel
     {
-        public string Nombre {get; set;}
-        public string Contrasena {get; set;}
-        public int Rol {get; set;}
-        public int Usuario {get; set;}
-    } 
+        public string Nombre { get; set; }
+        public string Contrasena { get; set; }
+        public int Rol { get; set; }
+        public int Usuario { get; set; }
+    }
     public class UpdateUsuariosModel
     {
-        public int Id {get; set;}
-        public string Nombre {get; set;}
-        public string Contrasena {get; set;}
-        public int Rol {get; set;}
-        public int Usuario {get; set;}
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Contrasena { get; set; }
+        public int Rol { get; set; }
+        public int Usuario { get; set; }
     }
     public class DeleteUsuariosModel
     {
-        public int Id {get; set;}
+        public int Id { get; set; }
     }
 }
