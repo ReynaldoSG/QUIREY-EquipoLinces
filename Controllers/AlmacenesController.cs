47838c9d667095ef83d993f9bdba58f45aec1d15
Jonathan Cortes <124106713+Arkan2601@users.noreply.github.com>
2024-07-18 22:45:36 -0600

    api

--- Controllers/AlmacenesController.cs
+++ Controllers/AlmacenesController.cs
@@ -74,7 +74,7 @@ namespace marcatel_api.Controllers
 
 
 
-        //[Authorize(AuthenticationSchemes = "Bearer")]
+        [Authorize(AuthenticationSchemes = "Bearer")]
 
         [HttpGet("Get")]
         public IActionResult GetAlmacenes()
