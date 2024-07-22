5bfddefe9358eb8e709570e294cd94d1e8ea857c
ReynaldoSG <124290617+ReynaldoSG@users.noreply.github.com>
2024-07-22 16:59:29 -0600

    empleados

--- Services/EmpleadosService.cs
+++ Services/EmpleadosService.cs
@@ -10,52 +10,52 @@ namespace marcatel_api.Services
 {
     public class EmpleadosService
     {
-        private  string connection;
+        private string connection;
         public EmpleadosService(IMarcatelDatabaseSetting settings)
         {
-             connection = settings.ConnectionString;
+            connection = settings.ConnectionString;
         }
 
 
-                public List<GetEmpleadosModel> GetEmpleados()
-                {
-                    ArrayList parametros = new ArrayList();
-                    ConexionDataAccess dac = new ConexionDataAccess(connection);
-                    var lista = new List<GetEmpleadosModel>();
-                    try
-                    {
-                        
-
-                        DataSet ds = dac.Fill("sp_GetEmpleado", parametros);
-                        if (ds.Tables[0].Rows.Count > 0)
-                        {
-                            foreach (DataRow row in ds.Tables[0].Rows)
-                            {
-                                lista.Add(new GetEmpleadosModel
-                                {
-                                    Id = int.Parse(row["Id"].ToString()),
-                                    Persona = row["Persona"].ToString(),
-                                    Sucursal = row["Sucursal"].ToString(),
-                                    Puesto = row["Puesto"].ToString(),
-                                    usuarioActualiza = row["usuarioActualiza"].ToString(),
-                                    fechaRegistro = DateTime.Parse(row["fechaRegistro"].ToString()),
-                                    fechaActualiza = DateTime.Parse(row["fechaActualiza"].ToString())
-                                    
-                                });
-                            }
-                        }
-                        return lista;
-                        
-                    }
-                    catch (Exception ex)
-                    {
-                        throw ex;
-                    }
-                    
-                    
-                }
-                public int InsertEmpleado(InsertEmpleadosModel empleadosModel)
+        public List<GetEmpleadosModel> GetEmpleados()
+        {
+            ArrayList parametros = new ArrayList();
+            ConexionDataAccess dac = new ConexionDataAccess(connection);
+            var lista = new List<GetEmpleadosModel>();
+            try
             {
+
+
+                DataSet ds = dac.Fill("sp_GetEmpleado", parametros);
+                if (ds.Tables[0].Rows.Count > 0)
+                {
+                    foreach (DataRow row in ds.Tables[0].Rows)
+                    {
+                        lista.Add(new GetEmpleadosModel
+                        {
+                            Id = int.Parse(row["Id"].ToString()),
+                            Persona = row["idPersona"].ToString(),
+                            Sucursal = row["Sucursal"].ToString(),
+                            Puesto = row["Puesto"].ToString(),
+                            usuarioActualiza = row["usuarioActualiza"].ToString(),
+                            fechaRegistro = DateTime.Parse(row["fechaRegistro"].ToString()),
+                            fechaActualiza = DateTime.Parse(row["fechaActualiza"].ToString())
+
+                        });
+                    }
+                }
+                return lista;
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+
+        }
+        public int InsertEmpleado(InsertEmpleadosModel empleadosModel)
+        {
             ArrayList parametros = new ArrayList();
             ConexionDataAccess dac = new ConexionDataAccess(connection);
             var lista = new List<GetEmpleadosModel>();
@@ -66,22 +66,22 @@ namespace marcatel_api.Services
                 parametros.Add(new SqlParameter { ParameterName = "@pIdSucursal", SqlDbType = SqlDbType.Int, Value = empleadosModel.IdSucursal });
                 parametros.Add(new SqlParameter { ParameterName = "@pIdPuesto", SqlDbType = SqlDbType.Int, Value = empleadosModel.IdPuesto });
                 parametros.Add(new SqlParameter { ParameterName = "@pUsuarioAct", SqlDbType = SqlDbType.Int, Value = empleadosModel.usuarioActualiza });
-              
+
 
                 DataSet ds = dac.Fill("sp_InsertEmpleado", parametros);
                 if (ds.Tables[0].Rows.Count > 0)
                 {
                     foreach (DataRow row in ds.Tables[0].Rows)
                     {
-                         lista.Add(new GetEmpleadosModel
-                                {
-                                  Persona = row["Persona"].ToString(),
-                                  Sucursal = row["Sucursal"].ToString(),
-                                  Puesto = row["Puesto"].ToString(),
-                                  fechaRegistro = DateTime.Parse(row["fechaRegistro"].ToString()),
-                                  fechaActualiza = DateTime.Parse(row["fechaActualiza"].ToString()),
-                                  usuarioActualiza = row["UsuarioActualiza"].ToString()
-                                });
+                        lista.Add(new GetEmpleadosModel
+                        {
+                            Persona = row["idPersona"].ToString(),
+                            Sucursal = row["Sucursal"].ToString(),
+                            Puesto = row["Puesto"].ToString(),
+                            fechaRegistro = DateTime.Parse(row["fechaRegistro"].ToString()),
+                            fechaActualiza = DateTime.Parse(row["fechaActualiza"].ToString()),
+                            usuarioActualiza = row["UsuarioActualiza"].ToString()
+                        });
                     }
                 }
                 return 1;
@@ -92,7 +92,7 @@ namespace marcatel_api.Services
                 return 0;
             }
         }
-           public int UpdateEmpleados(UpdateEmpleadosModel empleadosModel)
+        public int UpdateEmpleados(UpdateEmpleadosModel empleadosModel)
         {
             ArrayList parametros = new ArrayList();
             ConexionDataAccess dac = new ConexionDataAccess(connection);
@@ -100,7 +100,7 @@ namespace marcatel_api.Services
 
             try
             {
-                parametros.Add(new SqlParameter {ParameterName="@pId", SqlDbType = SqlDbType.Int, Value= empleadosModel.Id});
+                parametros.Add(new SqlParameter { ParameterName = "@pId", SqlDbType = SqlDbType.Int, Value = empleadosModel.Id });
                 parametros.Add(new SqlParameter { ParameterName = "@pIdPersona", SqlDbType = SqlDbType.Int, Value = empleadosModel.IdPersona });
                 parametros.Add(new SqlParameter { ParameterName = "@pIdSucursal", SqlDbType = SqlDbType.Int, Value = empleadosModel.IdSucursal });
                 parametros.Add(new SqlParameter { ParameterName = "@pIdPuesto", SqlDbType = SqlDbType.Int, Value = empleadosModel.IdPuesto });
@@ -112,13 +112,13 @@ namespace marcatel_api.Services
                     {
                         lista.Add(new GetEmpleadosModel
                         {
-                          Id = int.Parse(row["Id"].ToString()), 
-                          Persona = row["Persona"].ToString(),
-                          Sucursal = row["Sucursal"].ToString(),
-                          Puesto = row["Puesto"].ToString(),
-                          fechaRegistro = DateTime.Parse(row["fechaRegistro"].ToString()),
-                          fechaActualiza = DateTime.Parse(row["fechaActualiza"].ToString()),
-                          usuarioActualiza = row["UsuarioActualiza"].ToString()
+                            Id = int.Parse(row["Id"].ToString()),
+                            Persona = row["idPersona"].ToString(),
+                            Sucursal = row["Sucursal"].ToString(),
+                            Puesto = row["Puesto"].ToString(),
+                            fechaRegistro = DateTime.Parse(row["fechaRegistro"].ToString()),
+                            fechaActualiza = DateTime.Parse(row["fechaActualiza"].ToString()),
+                            usuarioActualiza = row["UsuarioActualiza"].ToString()
 
                         });
                     }
